ef1399fe762ce10e585bd3fa340b7ce606b11b5f
Huntertk <taufik.khan65@gmail.com>
2023-12-09 12:06:19 +0530

    first commit

---
+++ client/src/components/HomeTopContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/homeTopContainer.scss'
+
+const HomeTopContainer = () => {
+
+  return (
+    <section className='HomeTopContainer'>
+        <div className="content">
+            <h1>Book Aquaria KLCC Tickets</h1> 
+            <p>Travelvago is an authorized and trusted partner of the venue, offering curated experiences to enjoy this attraction.</p>
+        </div>
+        <div className="homeTopImgContainer">
+            <img src={"https://i.postimg.cc/0yL0bG9J/Whats-App-Image-2023-12-07-at-09-42-15-2.jpg"} alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default HomeTopContainer
---
+++ client/src/components/HomeWhyVisit.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/homeWhyVisit.scss'
+
+const HomeWhyVisit = () => {
+  
+  return (
+    <section className='whyVisitMainComponents'>
+        
+        <h1>Why Visit Genting SkyWorlds Theme Park</h1>
+        <div className="whyVisitCardContainer">
+            <div className="listData">
+
+                    <p>
+                    Aquaria KLCC is the top aquarium in South East Asia and Malaysia's FIRST underground aquarium. 
+                    </p>
+                    <p>
+                        Showcasing a 90-meter transparent tunnel pathway.
+                    </p>
+                    <p>
+                        This facility showcases over 5,000 exotic aquatic and terrestrial animalswithin their native environments.
+                    </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HomeWhyVisit
